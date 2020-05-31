cb15c23aaa4068bb230847ea5df09cbc060b3594
Mikhail19870105 <crazyhtc87@gmail.com>
2020-05-31 18:27:47 +0300

    Added PostManager.java

---
+++ src/main/java/ru/netology/manager/PostManager.java
@@ -0,0 +1,18 @@
+package ru.netology.manager;
+
+import ru.netology.domain.Post;
+
+public class PostManager {
+
+    private Post [] posts;
+
+    public Post [] search(int owner, String domain, String query, int ownersOnly, int count, int offset);
+    //TODO
+    return null;
+
+    public void delete(int ownerId, int postId);
+    //TODO
+    return null;
+
+
+}
